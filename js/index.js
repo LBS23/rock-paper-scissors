057d62983169de63a54bf32296d681ba01744d8a
HFSaruei <lcristobalbs@gmail.com>
2025-11-10 19:04:32 -0300

    Finished the game.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice() {
     let answer;
     answer = Math.floor(Math.random() * 3);
@@ -16,5 +17,37 @@ function getHumanChoice() {
     choice = prompt("Enter your choice: ")
     return choice;
 }
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice
+    computerChoice = computerChoice
+    if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } 
+    else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else if (computerChoice === humanChoice) {
+        console.log(`It's a draw! both players chose ${humanChoice}`);
+    }
+    else {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+}
+    for (let round = 0; round <5; round++){
+    playRound(getHumanChoice(),getComputerChoice())
+    }
+}
+
+playGame()
